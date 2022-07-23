77deb9dd51824ea3cd10321eb20d8a8f3a93818c
DESKTOP-RFSUQJ7\Marzuki. R <enalmarzuki07@gmail.com>
2022-07-23 19:30:31 +0800

    update state when search

--- src/Pages/Result/Result.tsx
+++ src/Pages/Result/Result.tsx
@@ -4,39 +4,43 @@ import IMGBurger from '../../Assets/image/img-burger.png';
 import IMGMyMusic from '../../Assets/image/img-mymusic.png';
 import IMGSearch from '../../Assets/image/img-search.png';
 import IMGClose from '../../Assets/image/img-close.png';
+import IMGLoading from '../../Assets/image/loading.gif';
 import Gap from '../../Components/Gap';
 import { CardMusic, ICardMusicProps } from './Components/CardMusic/CardMusic';
 import { BtnRndSecondary } from '../../Components/Button/BtnRndSecondary';
 import { Input } from '../../Components/Input/Input';
 import axios from 'axios';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 
 const Result = () => {
   const params = useParams();
   const [isShowModal, setIsShowModal] = useState(false);
   const [resultMusic, setResultMusic] = useState<any>();
-  const [limit, setLimit] = useState(25);
+  const [limit, setLimit] = useState(10);
   const [keyword, setKeyword] = useState(params.key);
-  const [tempKeyword, settempKeyword] = useState('');
-
-  console.log('params', params);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getListMusic = () => {
+    setIsLoading(true);
     axios
-      .get(`https://itunes.apple.com/search?term=${keyword}&limit=${limit}`)
+      .get(
+        `https://itunes.apple.com/search?term=${keyword}&limit=${limit}&entity=musicVideo`
+      )
       .then((res) => setResultMusic(res.data.results))
-      .catch((err) => alert(err));
+      .catch((err) => alert(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  const onClickSearch = () => {
+    setResultMusic(undefined);
+    setLimit(10);
+    getListMusic();
+    setIsShowModal(false);
   };
 
   useEffect(() => {
     getListMusic();
-  }, [keyword, limit]);
-
-  console.log('resultMusic', resultMusic);
-
-  if (!resultMusic) {
-    return <p>loading...</p>;
-  }
+  }, [limit]);
 
   return (
     <div className={Styles['container']}>
@@ -50,61 +54,73 @@ const Result = () => {
         />
       </div>
 
-      <Gap height={50} />
-      <div className={Styles['search-key-wrapper']}>
-        <p className={Styles['search-key-title']}>Search result for :</p>
-        <p className={Styles['search-key']}>{keyword}</p>
-      </div>
-
-      <div>
-        {resultMusic.map((item: any) => (
-          <CardMusic
-            key={item.artistId}
-            name={item.artistName}
-            title={item.collectionName}
-            img={item.artworkUrl100}
-            price={item.collectionPrice}
-            genre={item.primaryGenreName}
-          />
-        ))}
-      </div>
-
-      <div className={Styles['load-wrapper']}>
-        <button
-          className={Styles['btn-rnd-load-more']}
-          onClick={() => setLimit((prev) => prev + 25)}
-        >
-          Load More
-        </button>
-      </div>
-
-      <Gap height={20} />
-
-      {isShowModal && (
-        <div className={Styles['modal-wrapper']}>
-          <img
-            src={IMGClose}
-            alt="img-close.png"
-            className={Styles['modal-ic-close']}
-            onClick={() => setIsShowModal(false)}
-          />
-          <p className={Styles['modal-title']}>Search</p>
-          <Gap height={31} />
-          <div className={Styles['form-wrapper']}>
-            <Input
-              placeholder="Artist / Album / Title"
-              onChange={(e) => settempKeyword(e.target.value)}
-            />
-            <Gap height={15} />
-            <BtnRndSecondary
-              title="Search"
-              onClick={() => {
-                setKeyword(tempKeyword);
-                setIsShowModal(false);
-              }}
+      {!resultMusic ? (
+        <>
+          <Gap height={50} />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <img
+              style={{ width: 50, height: 50 }}
+              src={IMGLoading}
+              alt="ladong.png"
             />
           </div>
-        </div>
+        </>
+      ) : (
+        <>
+          <Gap height={50} />
+          <div className={Styles['search-key-wrapper']}>
+            <p className={Styles['search-key-title']}>Search result for :</p>
+            <p className={Styles['search-key']}>{keyword}</p>
+          </div>
+
+          <div>
+            {resultMusic.map((item: any) => (
+              <CardMusic
+                key={item.artistId}
+                name={item.artistName}
+                title={item.collectionName}
+                img={item.artworkUrl100}
+                price={item.collectionPrice}
+                genre={item.primaryGenreName}
+              />
+            ))}
+          </div>
+
+          <div className={Styles['load-wrapper']}>
+            <button
+              className={Styles['btn-rnd-load-more']}
+              onClick={() => setLimit((prev) => prev + 10)}
+            >
+              Load More
+            </button>
+          </div>
+
+          <Gap height={20} />
+
+          {isShowModal && (
+            <div className={Styles['modal-wrapper']}>
+              <img
+                src={IMGClose}
+                alt="img-close.png"
+                className={Styles['modal-ic-close']}
+                onClick={() => setIsShowModal(false)}
+              />
+              <p className={Styles['modal-title']}>Search</p>
+              <Gap height={31} />
+              <div className={Styles['form-wrapper']}>
+                <Input
+                  placeholder="Artist / Album / Title"
+                  onChange={(e) => setKeyword(e.target.value)}
+                />
+                <Gap height={15} />
+                <BtnRndSecondary
+                  title="Search"
+                  onClick={() => onClickSearch()}
+                />
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
